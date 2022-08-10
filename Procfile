fa643486bc585ebff12ca0c582b4e8d242145967
taemoohyun <taemoohyun@naver.com>
2022-08-10 11:12:50 +0900

    setting update

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-web: heroku config:set DEBUG_COLLECTSTATIC=1
 release: python manage.py migrate
 web: gunicorn djangoapp_test1.wsgi --log-file=-
--- djangoapp_test1/settings.py
+++ djangoapp_test1/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
